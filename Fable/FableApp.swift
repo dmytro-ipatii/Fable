2bffcb597ffd3cb65001fb859c788e2b3bbcddca
Dmytro Ipatii <dima.ipatjj@gmail.com>
2026-06-16 07:35:29 +0200

    Initial Commit

---
+++ Fable/FableApp.swift
@@ -0,0 +1,17 @@
+//
+//  FableApp.swift
+//  Fable
+//
+//  Created by Dmytro Ipatii on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct FableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
